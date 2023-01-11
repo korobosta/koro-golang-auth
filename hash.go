b2791fdb61c6ac8151499083c8fc57d099920908
Kevin Ongulu <kevinkorobosta@gmail.com>
2023-01-11 14:04:25 +0300

    update authenticate user

--- hash.go
+++ hash.go
@@ -5,7 +5,7 @@ import (
     "golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) (string) {
+func HashPassword(password string) string {
     bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
     if err != nil {
         panic(err.Error())
